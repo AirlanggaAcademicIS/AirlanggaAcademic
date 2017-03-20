7e5f08c8c8c0a456528b37d24f29e8ed169f9f29
Kafifahziyani <ziyanikafifah@gmail.com>
2017-03-20 23:42:12 +0700

    [mahasiswa]tambah form prestasi

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,18 @@ Route::group(['middleware' => 'auth'], function () {
 //        // Uses Auth Middleware
 //    });
 
+Route::get('/mahasiswa/prestasi', function () {
+    return view('mahasiswa.tabel');
+});
+Route::get('/mahasiswa/input_prestasi', function () {
+    return view('mahasiswa.input_prestasi');
+});
+Route::get('/mahasiswa/edit_prestasi', function () {
+    return view('mahasiswa.edit_prestasi');
+});
+Route::get('/mahasiswa/tabel_berhasil', function () {
+    return view('mahasiswa.tabel_berhasil');
+});
     //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
     #adminlte_routes
 });
