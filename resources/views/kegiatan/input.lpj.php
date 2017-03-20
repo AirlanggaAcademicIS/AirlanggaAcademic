53f3ae760d96402d54118d6cf99b2ffc306312e6
hestiindah <hestindh@gmail.com>
2017-03-21 05:45:32 +0700

    Input LPJ

--- resources/views/kegiatan/input.lpj.php
+++
@@ -1,6 +0,0 @@
-<label>Nomor Kegiatan:</label>
-      <textarea style="width: 150px" type="text" class="form-control" id="nomor" placeholder="Nomor Kegiatan"></textarea>
-    </div>
-<label>Evaluasi Kegiatan</label>
-      <textarea style="width: 500px" type="text" class="form-control" rows="3" placeholder="Evaluasi Kegiatan"></textarea>
-        </div>
