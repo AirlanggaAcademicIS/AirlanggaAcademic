ddeedadec89bc41e97bbe39925ee825db0746408
fariechusmana <chusmanaarie@gmail.com>
2017-04-09 18:39:28 +0700

    CRUD Konferensi Dosen

--- routes/web.php
+++ routes/web.php
@@ -78,7 +78,23 @@ Route::group(['middleware' => ['auth']], function () {
 
     // Modul Dosen
     Route::group(['prefix' => 'dosen'], function() {
-            
+            // Menampilkan tabel
+        Route::get('konferensi','Dosen\KonferensiController@index');
+
+        // Menampilkan form tambah biodata
+        Route::get('konferensi/create','Dosen\KonferensiController@create');
+
+        // Menambahkan form yg di isi tadi ke tabel biodata
+        Route::post('konferensi/create','Dosen\KonferensiController@createAction');
+
+        // Menghapus biodata sesuai id yang dipilih
+        Route::get('konferensi/{id}/delete','Dosen\KonferensiController@delete');
+
+        // Menampilkan form edit biodata dari id yg dipilih
+        Route::get('konferensi/{id}/edit','Dosen\KonferensiController@edit');
+
+        // Mengupdate biodata dengan isi dari form
+        Route::post('konferensi/{id}/edit','Dosen\KonferensiController@editAction');
     });
 
     // Modul Kegiatan
