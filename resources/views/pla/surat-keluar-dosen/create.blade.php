4c1a7692d3eba5f344f8d1d87b624bcecbfb0ada
Kurnia Wahyu <kurniawahyu59@gmail.com>
2017-04-11 08:16:46 +0700

    surat keluar dosen and adding storage folder on .gitginore

---
+++ resources/views/pla/surat-keluar-dosen/create.blade.php
@@ -0,0 +1,91 @@
+@extends('adminlte::layouts.app')
+
+@section('htmlheader_title')
+Tambah Surat Keluar Dosen
+@endsection
+
+@section('contentheader_title')
+Tambah Surat Keluar Dosen
+@endsection
+
+@section('code-header')
+
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"></script> 
+<link rel="stylesheet" href="{{ asset('/css/dropzone.css') }}">
+
+@endsection
+
+@section('main-content')
+<style>
+	.form-group label{
+		text-align: left !important;
+	}
+</style>
+	<!-- Ini buat menampilkan notifikasi -->
+	@foreach (['danger', 'warning', 'success', 'info'] as $msg)
+	@if(Session::has('alert-' . $msg))
+<div class="alert alert-{{ $msg }}">
+	<p class="" style="border-radius: 0">{{ Session::get('alert-' . $msg) }}</p>
+</div>
+	{!!Session::forget('alert-' . $msg)!!}
+	@endif
+	@endforeach
+
+
+<div class="row">
+	<div class="col-md-12">
+		<div class="">
+
+			@if (count($errors) > 0)
+			<div class="alert alert-danger">
+				<ul>
+					@foreach ($errors->all() as $error)
+					<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div>
+			@endif
+			<br>
+			<form id="tambahJadwalPermohonan" method="post" action="{{url('/pla/surat-keluar-dosen/create')}}" enctype="multipart/form-data"  class="form-horizontal">
+				<input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+				<!-- Menampilkan input text biasa -->
+				<div class="form-group">
+					<label for="id_permohonan_ruang" class="col-sm-2 control-label">Nama</label>
+					<div class="col-md-8">
+						<input type="text" class="form-control input-lg" id="id_permohonan_ruang" name="id_permohonan_ruang" placeholder="Masukkan nama" required>
+					</div>
+				</div>
+
+				<div class="form-group">
+					<label for="id_ruang" class="col-sm-2 control-label">Tanggal Upload</label>
+					<div class="col-md-3">
+						<input type="text" class="form-control input-lg" id="id_ruang" name="id_ruang" required>
+					</div>
+				</div>
+
+
+				<div class="form-group text-center">
+					<div class="col-md-8 col-md-offset-2">
+					<button type="submit" class="btn btn-primary btn-lg">
+							Confirm
+						</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+@endsection
+
+@section('code-footer')
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+$( function() {
+    var date = $('#datepicker').datepicker({ dateFormat: 'yy/mm/dd' }).val();
+
+  } );
+  </script>
+@endsection
+
