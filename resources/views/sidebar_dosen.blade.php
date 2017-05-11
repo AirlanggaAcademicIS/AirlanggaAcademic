efee10726c4a7574b86aafdccb828ea0f9e5c6e4
AlbertMartinHau <albertmartyn315@gmail.com>
2017-05-11 19:56:17 +0700

    CRUD Capaian Program

--- resources/views/sidebar_dosen.blade.php
+++ resources/views/sidebar_dosen.blade.php
@@ -6,3 +6,10 @@
 >
 <a href="{{ url('transaksi') }}"><i class="fa fa-calculator" aria-hidden="true"></i>Transaksi</a>
 </li> -->
+            <li
+            @if($page == 'capaian-program')
+            {!! 'class="active"'!!}
+            @endif
+            >
+            <!-- Href menuju ke url kurikulum/capaian-program -->
+            <a href="{{ url('dosen/kurikulum/cp_program') }}"><i class='fa fa-book'></i> <span> Capaian Program</span></a>
--- routes/web.php
+++ routes/web.php
@@ -29,9 +29,31 @@ Route buat mahasiswa ditaruh dibawah sini
 Route buat dosen ditaruh dibawah sini
 =========================================
 */
+   // Menampilkan form tambah biodata
+// Modul Kurikulum
+Route::group(['prefix' => 'dosen'], function() {
+	Route::group(['prefix' => 'kurikulum'], function() {
+		Route::group(['prefix' => 'cp_program'], function() {
 
+			Route::get('/','Dosen\Kurikulum\CapaianProgramController@index');
 
+	        Route::get('create','Dosen\Kurikulum\CapaianProgramController@create');
 
+	        // Menambahkan form yg di isi tadi ke tabel biodata
+	        Route::post('create','Dosen\Kurikulum\CapaianProgramController@createAction');
+
+	        // Menghapus biodata sesuai id yang dipilih
+	        Route::get('{id}/delete','Dosen\Kurikulum\CapaianProgramController@delete');
+
+	        // Menampilkan form edit biodata dari id yg dipilih
+	        Route::get('{id}/edit','Dosen\Kurikulum\CapaianProgramController@edit');
+
+	        // Mengupdate biodata dengan isi dari form
+	        Route::post('{id}/edit','Dosen\Kurikulum\CapaianProgramController@editAction');
+
+		});
+	});
+});
 /*
 ==========================================
 Route buat karyawan ditaruh dibawah sini
