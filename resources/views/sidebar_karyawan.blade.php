50678bc599a3c82ab6dfbd9a29eb10c9f709843b
mryusuf <m.kincop@gmail.com>
2017-05-11 20:28:18 +0700

    karyawan-verifikasi mahasiswa

--- resources/views/sidebar_karyawan.blade.php
+++ resources/views/sidebar_karyawan.blade.php
@@ -6,3 +6,10 @@
 >
 <a href="{{ url('transaksi') }}"><i class="fa fa-calculator" aria-hidden="true"></i>Transaksi</a>
 </li> -->
+<li
+@if($page == 'verifikasi')
+{!! 'class="active"'!!}
+@endif
+>
+<a href="{{ url('karyawan/verifikasi') }}"><i class="fa fa-calculator" aria-hidden="true"></i>Verifikasi Mahasiswa</a>
+</li>
--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,23 @@ Route buat dosen ditaruh dibawah sini
 Route buat karyawan ditaruh dibawah sini
 =========================================
 */
+Route::group(['prefix' => 'karyawan'], function() {
+	// Menampilkan tabel
+        Route::get('verifikasi','Karyawan\VerifikasiController@index');
+        Route::get('verifikasi/penelitian','Karyawan\VerifikasiController@createPenelitian');
+
+        Route::get('verifikasi/prestasi','Karyawan\VerifikasiController@createPrestasi');
 
 
+        Route::get('verifikasi/{id}/penelitian','Karyawan\VerifikasiController@editPenelitian');
 
+        Route::post('verifikasi/{id}/penelitian','Karyawan\VerifikasiController@editPenelitianAction');
+
+        Route::get('verifikasi/{id}/prestasi','Karyawan\VerifikasiController@editPrestasi');
+        Route::post('verifikasi/{id}/prestasi','Karyawan\VerifikasiController@editPrestasiAction');
+
+
+        Route::get('verifikasi/download/{id}','Karyawan\VerifikasiController@download');
+
+	});
 });
