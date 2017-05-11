a63a86f152403738d903638505593d6440c8ecf6
rizaldifirdaus <rizaldilucu@gmail.com>
2017-05-11 20:18:42 +0700

    [kurikulum] CRUD kategori cp pembelajaran

--- resources/views/sidebar_dosen.blade.php
+++ resources/views/sidebar_dosen.blade.php
@@ -6,3 +6,16 @@
 >
 <a href="{{ url('transaksi') }}"><i class="fa fa-calculator" aria-hidden="true"></i>Transaksi</a>
 </li> -->
+
+<li>
+<a href=""><i class='fa fa-book'></i> <span> Kurikulum</span></a>
+<ul class="treeview-menu">
+</li>
+
+<li
+@if($page == 'kode')
+{!! 'class="active"'!!}
+@endif
+>
+<a href="{{ url('dosen/kurikulum/kodecppem') }}"><i class="fa fa-book" aria-hidden="true"></i>Kode CP Pembelajaran</a>
+</li>
--- routes/web.php
+++ routes/web.php
@@ -29,15 +29,48 @@ Route buat mahasiswa ditaruh dibawah sini
 Route buat dosen ditaruh dibawah sini
 =========================================
 */
+// Modul Kurikulum
+Route::group(['prefix' => 'dosen'], function() {
 
+	Route::group(['prefix' => 'kurikulum'], function() {
 
+		Route::group(['prefix' => 'kodecppem'], function() {
 
-/*
+		    Route::get('/','Dosen\Kurikulum\KodeController@index');
+
+		    Route::post('create','Dosen\Kurikulum\KodeController@createAction');
+
+		    Route::get('/delete/{id}','Dosen\Kurikulum\KodeController@delete');
+
+		    Route::get('/edit/{id}','Dosen\Kurikulum\KodeController@edit');
+
+		    Route::post('/edit/{id}','Dosen\Kurikulum\KodeController@editAction');
+		});
+
+	});
+        });
+
+        /*
 ==========================================
 Route buat karyawan ditaruh dibawah sini
 =========================================
 */
+//modul mahasiswa
+Route::group(['prefix' => 'karyawan'], function() {
 
 
+        // Menampilkan tabel
+        Route::get('akun','Karyawan\AkunMahasiswaController@index');
+        // Menampilkan form tambah biodata
+        Route::get('akun/create','Karyawan\AkunMahasiswaController@create');
+        // Menambahkan form yg di isi tadi ke tabel biodata
+        Route::post('akun/create','Karyawan\AkunMahasiswaController@createAction');
+        // Menghapus biodata sesuai id yang dipilih
+        Route::get('akun/{nim}/delete','Karyawan\AkunMahasiswaController@delete');
+        // Menampilkan form edit biodata dari id yg dipilih
+        Route::get('akun/{nim}/edit','Karyawan\AkunMahasiswaController@edit');
+        // Mengupdate biodata dengan isi dari form
+        Route::post('akun/{nim}/edit','Karyawan\AkunMahasiswaController@editAction');
+        });
 
 });
