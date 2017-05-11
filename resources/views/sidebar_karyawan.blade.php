9b56717397c9f21f7eea4842ef27c726ea5ccab3
kretawiweka <ekanuraga@gmail.com>
2017-05-11 17:52:25 +0700

    pdf and crud matkul -kurikulum

--- resources/views/sidebar_karyawan.blade.php
+++ resources/views/sidebar_karyawan.blade.php
@@ -6,3 +6,22 @@
 >
 <a href="{{ url('transaksi') }}"><i class="fa fa-calculator" aria-hidden="true"></i>Transaksi</a>
 </li> -->
+
+
+<li class="treeview" id="scrollspy-components"
+@if($page == 'mata-kuliah')
+{!! 'class="active"'!!}
+@endif
+>
+>
+	<a href="#"><i class="fa fa-circle-o"></i> Kurikulum</a>
+	<ul class="nav treeview-menu">
+		<li
+		@if($page == 'mata-kuliah')
+		{!! 'class="active"'!!}
+		@endif
+		>
+		<a href="{{ url('karyawan/kurikulum/mata-kuliah') }}"><i class="fa fa-circle-o" aria-hidden="true"></i>Mata Kuliah</a>
+		</li>
+	</ul>
+</li>
--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,6 @@ Route buat mahasiswa ditaruh dibawah sini
 */
 
 
-
 /*
 ==========================================
 Route buat dosen ditaruh dibawah sini
@@ -37,7 +36,20 @@ Route buat dosen ditaruh dibawah sini
 Route buat karyawan ditaruh dibawah sini
 =========================================
 */
+    Route::group( ['prefix'=>'karyawan'], function(){
+	    Route::group( ['prefix'=>'kurikulum'], function(){
+	
+		    Route::group( ['prefix'=>'mata-kuliah'], function(){
+		        Route::get('/',['as'=>'karyawan.kurikulum.mata-kuliah.index', 'uses'=>'Karyawan\Kurikulum\MataKuliahController@index']);
+		        Route::get('/print/{id}',['as'=>'karyawan.kurikulum.mata-kuliah.index', 'uses'=>'Karyawan\Kurikulum\MataKuliahController@toPdf']);		        
+		        Route::get('/create',['as'=>'karyawan.kurikulum.mata-kuliah.index', 'uses'=>'Karyawan\Kurikulum\MataKuliahController@create']);		        
+		        Route::post('/create',['as'=>'karyawan.kurikulum.mata-kuliah.index', 'uses'=>'Karyawan\Kurikulum\MataKuliahController@createAction']);
+		        Route::get('/edit/{id}',['as'=>'karyawan.kurikulum.mata-kuliah.index', 'uses'=>'Karyawan\Kurikulum\MataKuliahController@edit']);		        
+		        Route::post('/edit/{id}',['as'=>'karyawan.kurikulum.mata-kuliah.index', 'uses'=>'Karyawan\Kurikulum\MataKuliahController@editAction']);
+		        Route::get('/delete{id}',['as'=>'karyawan.kurikulum.mata-kuliah.index', 'uses'=>'Karyawan\Kurikulum\MataKuliahController@delete']);		        		        		        
+		    });
 
-
+	    });
+    });
 
 });
