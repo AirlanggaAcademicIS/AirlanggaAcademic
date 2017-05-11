c5aeb50fbf77aa9e5d0fd4000516eafd9fa6522e
Regina Devi Loanita <Regina Devi Loanita>
2017-05-11 16:48:41 +0700

    Add KBK

--- resources/views/sidebar_karyawan.blade.php
+++ resources/views/sidebar_karyawan.blade.php
@@ -6,3 +6,15 @@
 >
 <a href="{{ url('transaksi') }}"><i class="fa fa-calculator" aria-hidden="true"></i>Transaksi</a>
 </li> -->
+<li>
+	<a href=""><i class='fa fa-users'></i> <span> Monitoring Skripsi</span></a>
+	<ul class="treeview-menu">
+		<li
+		@if($page == 'skripsi')
+		{!! 'claass="active"'!!}
+		@endif>
+
+		<a href="{{url('karyawan/monitoring-skripsi/KBK')}}"><i class='fa fa-book'></i><span> KBK</span>
+		</li>
+	</ul>
+</li>
--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,22 @@ Route buat dosen ditaruh dibawah sini
 Route buat karyawan ditaruh dibawah sini
 =========================================
 */
+		Route::get('karyawan/monitoring-skripsi/KBK','karyawan\monitoringskripsi\KBKController@index');
+
+        // Menampilkan form tambah biodata
+        Route::get('karyawan/monitoring-skripsi/KBK/create','karyawan\monitoringskripsi\KBKController@create');
+
+        // Menambahkan form yg di isi tadi ke tabel biodata	
+        Route::post('karyawan/monitoring-skripsi/KBK/create','karyawan\monitoringskripsi\KBKController@createAction');
+
+        // Menghapus biodata sesuai id yang dipilih
+        Route::get('karyawan/monitoring-skripsi/KBK/{id_kbk}/delete','karyawan\monitoringskripsi\KBKController@delete');
+
+        // Menampilkan form edit biodata dari id yg dipilih
+        Route::get('karyawan/monitoring-skripsi/KBK/{id_kbk}/edit','karyawan\monitoringskripsi\KBKController@edit');
+
+        // Mengupdate biodata dengan isi dari form
+        Route::post('karyawan/monitoring-skripsi/KBK/{id_kbk}/edit','karyawan\monitoringskripsi\KBKController@editAction');
 
 
 
