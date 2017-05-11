0c047e7115de975a988f1b0a1c72c46933730724
fandi bram <fandi bram>
2017-05-11 22:43:49 +0700

    karyawan akun mahasiswa

--- resources/views/sidebar_karyawan.blade.php
+++ resources/views/sidebar_karyawan.blade.php
@@ -6,3 +6,10 @@
 >
 <a href="{{ url('transaksi') }}"><i class="fa fa-calculator" aria-hidden="true"></i>Transaksi</a>
 </li> -->
+<li
+@if($page == 'AkunMahasiswa')
+{!! 'class="active"'!!}
+@endif
+>
+<a href="{{ url('karyawan/akun') }}"><i class="fa fa-calculator" aria-hidden="true"></i>Akun Mahasiswa</a>
+</li>
--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,21 @@ Route buat dosen ditaruh dibawah sini
 Route buat karyawan ditaruh dibawah sini
 =========================================
 */
-
-
+		
+		Route::group(['prefix' => 'karyawan'], function() {
+		
+		// Menampilkan tabel
+        Route::get('akun','Karyawan\AkunMahasiswaController@index');
+        // Menampilkan form tambah biodata
+        Route::get('akun/create','Karyawan\AkunMahasiswaController@create');
+        // Menambahkan form yg di isi tadi ke tabel biodata
+        Route::post('akun/create','Karyawan\AkunMahasiswaController@createAction');
+        // Menghapus biodata sesuai id yang dipilih
+        Route::get('akun/{nim}/delete','Karyawan\AkunMahasiswaController@delete');
+        // Menampilkan form edit biodata dari id yg dipilih
+        Route::get('akun/{nim}/edit','Karyawan\AkunMahasiswaController@edit');
+        // Mengupdate biodata dengan isi dari form
+        Route::post('akun/{nim}/edit','Karyawan\AkunMahasiswaController@editAction');
+        });
 
 });
