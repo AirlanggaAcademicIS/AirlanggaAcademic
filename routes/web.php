4b8f35db9713a3b027748afa45c4c68cc843e9ae
Andhini Dita <Andhini Dita>
2017-05-12 00:29:33 +0700

    [Notulensi]View Daftar Rapat

--- routes/web.php
+++ routes/web.php
@@ -20,24 +20,34 @@ Route::get('/logout', 'Auth\LoginController@logout');
 ==========================================
 Route buat mahasiswa ditaruh dibawah sini
 =========================================
+
 */
 
 
 
-/*
-==========================================
-Route buat dosen ditaruh dibawah sini
-=========================================
-*/
+
+//Route buat dosen ditaruh dibawah sini
+Route::group(['prefix' => 'notulensi'], function() {
+//Lihat daftar hasil rapat
+Route::get('','Dosen\NotulensiController@index');
+// Menampilkan form edit notulensi rapat dari id yg dipilih
+Route::get('notulensi/{id_notulen}/LihatHasilRapat','Dosen\NotulensiController@edit');
+// Mengupdate notulensi rapat dengan isi dari form
+Route::post('notulensi/{id_notulen}/LihatHasilRapat','Dosen\NotulensiController@editAction');
 
 
 
-/*
-==========================================
-Route buat karyawan ditaruh dibawah sini
-=========================================
-*/
+
+
+
+//Route buat karyawan ditaruh dibawah sini
+Route::group(['prefix' => 'notulensi'], function() {
+//Lihat daftar hasil rapat
+Route::get('','Karyawan\NotulensiController@index');
 
 
 
 });
+});
+});
+
