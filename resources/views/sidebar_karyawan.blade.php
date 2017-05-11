863a967c59eda166995f9aa4c70f29ff0f873e05
dzikriusammah <dzikrirobbi4@gmail.com>
2017-05-11 17:32:31 +0700

    Tambah pilihan status

--- resources/views/sidebar_karyawan.blade.php
+++ resources/views/sidebar_karyawan.blade.php
@@ -6,3 +6,14 @@
 >
 <a href="{{ url('transaksi') }}"><i class="fa fa-calculator" aria-hidden="true"></i>Transaksi</a>
 </li> -->
+ <li>
+            <a href=""><i class='fa fa-users'></i> <span> Monitoring Skripsi</span></a>
+            <ul class="treeview-menu">
+            <!-- Sidebarnya ditaruh dibawah sini -->
+            <li
+            @if($page == 'status')
+            {!! 'class="active"'!!}
+            @endif>
+
+            <a href="{{ url('/karyawan/monitoring-skripsi/status') }}"><i class='fa fa-book'></i><span>status</span></a>
+            </li>
--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,21 @@ Route buat dosen ditaruh dibawah sini
 Route buat karyawan ditaruh dibawah sini
 =========================================
 */
+		Route::get('karyawan/monitoring-skripsi/status','karyawan\monitoringskripsi\StatusController@index');
+ // Menampilkan form tambah biodata
+        Route::get('karyawan/monitoring-skripsi/status/create','karyawan\monitoringskripsi\StatusController@create');
 
+        // Menambahkan form yg di isi tadi ke tabel biodata
+        Route::post('karyawan/monitoring-skripsi/status/create','karyawan\monitoringskripsi\StatusController@createAction');
+
+        // Menghapus biodata sesuai id yang dipilih
+        Route::get('karyawan/monitoring-skripsi/status/{id_kbk}/delete','karyawan\monitoringskripsi\StatusController@delete');
+
+        // Menampilkan form edit biodata dari id yg dipilih
+        Route::get('karyawan/monitoring-skripsi/status/{id_kbk}/edit','karyawan\monitoringskripsi\StatusController@edit');
+
+        // Mengupdate biodata dengan isi dari form
+        Route::post('karyawan/monitoring-skripsi/status/{id_kbk}/edit','karyawan\monitoringskripsi\StatusController@editAction');
 
 
 });
