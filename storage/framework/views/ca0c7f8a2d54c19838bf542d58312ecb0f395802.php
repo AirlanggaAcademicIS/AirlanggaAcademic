ef24e96a48b1b7b0c838e764d41ef0f6f7a17915
rizqiromdloni <rizqi.romdloni@gmail.com>
2017-05-19 06:00:52 +0700

    [Notulensi] Notulensi Karyawan

--- storage/framework/views/ca0c7f8a2d54c19838bf542d58312ecb0f395802.php
+++ storage/framework/views/ca0c7f8a2d54c19838bf542d58312ecb0f395802.php
@@ -9,19 +9,42 @@
 
 <!-- JQuery and bootstrap are required by Laravel 5.3 in resources/assets/js/bootstrap.js-->
 <!-- Laravel App -->
-<script src="<?php echo e(asset('/js/app.js')); ?>" type="text/javascript"></script>
-<link href="http://cdn.datatables.net/1.10.13/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css" />
-<script src="http://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js" type="text/javascript"></script>
-<link href="<?php echo e(asset('/css/lightbox.css')); ?>" rel="stylesheet">
-<link href="<?php echo e(asset('/css/font-awesome.css')); ?>" rel="stylesheet">
+<script src="<?php echo e(mix('/js/app.js')); ?>" type="text/javascript"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"></script>
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- JQuery and bootstrap are required by Laravel 5.3 in resources/assets/js/bootstrap.js-->
+<!-- Laravel App -->
+
+<script src="<?php echo e(asset('plugins/jquery.dataTables.js')); ?>" type="text/javascript"></script>
+<script src="<?php echo e(asset('plugins/dataTables.bootstrap.js')); ?>" type="text/javascript"></script>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <link href="<?php echo e(asset('/css/bootstrap-tagsinput.css')); ?>" rel="stylesheet">
-<script src="<?php echo e(asset('/js/lightbox.js')); ?>"></script>
-<script src="<?php echo e(asset('/js/bootstrap-tagsinput.js')); ?>"></script>
-<script src="<?php echo e(asset('/js/bootstrap-tagsinput-angular.js')); ?>"></script>
-<!-- Optionally, you can add Slimscroll and FastClick plugins.
-      Both of these plugins are recommended to enhance the
-      user experience. Slimscroll is required when using the
-      fixed layout. -->
+<script type="text/javascript" src="<?php echo e(asset('/js/bootstrap-tagsinput.js')); ?>"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+<script src="<?php echo e(mix('/js/app.js')); ?>" type="text/javascript"></script>
+<!-- <script src="<?php echo e(asset('/plugins/jquery-2.2.3.min.js')); ?>" type="text/javascript"></script> -->
+<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+<script src="http://code.jquery.com/ui/1.11.0/jquery-ui.js"></script>
+<script src="<?php echo e(asset('/plugins/bootstrap-datepicker.js')); ?>"></script>
+<script src="<?php echo e(asset('/js/app.js')); ?>" type="text/javascript"></script>
+<<<<<<< HEAD
+=======
+
+>>>>>>> d1777ed63d6148f51abc8d4c913f6785d88e0cf1
 <script>
     window.Laravel = <?php echo json_encode([
         'csrfToken' => csrf_token(),
