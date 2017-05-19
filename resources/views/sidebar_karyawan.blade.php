742bbfdfad24cff2c8cf3f433aa8c2edd6f9c11d
elzhaodie <elzhaodies@gmail.com>
2017-05-19 07:45:11 +0700

    notulensi rapat gui tabel daftar kehadiran rapat

--- resources/views/sidebar_karyawan.blade.php
+++ resources/views/sidebar_karyawan.blade.php
@@ -7,19 +7,44 @@
 
 <a href="{{ url('transaksi') }}"><i class="fa fa-calculator" aria-hidden="true"></i>Transaksi</a>
 </li> -->
+
+<!--Awal Notulen-->
+<li>
+            <a href=""><i class='fa fa-users'></i> <span> Notulensi</span></a>
+            <ul class="treeview-menu">
+            <!-- Sidebarnya ditaruh dibawah sini -->
+
+            <li
+            @if($page == 'notulensi')
+            {!! 'class="active"'!!}
+            @endif
+            >
+            <!-- Href menuju ke url notulensi/notulensi rapat -->
+            <a href="#"><i class='fa fa-book'></i> <span>Undangan</span></a>
+            <a href="{{ url('dosenrapat') }}"><i class='fa fa-book'></i> <span>Kehadiran Rapat</span></a>
+            <a href="#"><i class='fa fa-book'></i> <span>Notulensi</span></a>
+            <a href="#"><i class='fa fa-book'></i> <span>Kalender</span></a>
+            </li>
+            </ul>
+        </li>
+<!--Akhir Notulen-->
+
+
 <li>
  <a href=""><i class='fa fa-users'></i> <span> Monitoring Skripsi</span></a>
  <ul class="treeview-menu">
   <li
   @if($page == 'skripsi')
-  {!! 'claass="active"'!!}
+  {!! 'class="active"'!!}
   @endif>
 
   <a href="{{url('karyawan/monitoring-skripsi/KBK')}}"><i class='fa fa-book'></i><span> KBK</span>
   </a>
 </li>
+</ul>
+</li>
 
-			<li>
+            <li>
             <a href=""><i class='fa fa-users'></i> <span> Notulensi</span></a>
             <ul class="treeview-menu">
             <!-- Sidebarnya ditaruh dibawah sini -->
@@ -34,6 +59,7 @@
             <a href="{{ url('notulensi') }}"><i class='fa fa-book'></i> <span>Notulensi</span></a>
             <a href="#"><i class='fa fa-book'></i> <span>Kalender</span></a>
             </li>
+            </ul>
         </li>
 
 <li
