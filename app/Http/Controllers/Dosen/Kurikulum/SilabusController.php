b26bbc84ed6dbe4962bb258ec11d0914009c6a44
kretawiweka <ekanuraga@gmail.com>
2017-05-22 20:01:38 +0700

    create silabus

--- app/Http/Controllers/Dosen/Kurikulum/SilabusController.php
+++ app/Http/Controllers/Dosen/Kurikulum/SilabusController.php
@@ -19,6 +19,9 @@ use App\Silabus_cp_matkul;
 use App\Silabus_detail_media;
 use App\Silabus_Sistem_Pembelajaran;
 use App\Silabus_Koor_Matkul;
+use App\Silabus_Media_Pembelajaran;
+use App\Status_Team_Teaching;
+
 
 class SilabusController extends Controller
 {
@@ -43,7 +46,12 @@ class SilabusController extends Controller
         $data = [
             // Buat di sidebar, biar ketika diklik yg aktif sidebar biodata
             'page' => 'silabus',
-            'mata_kuliah' => Silabus_Matkul::all()            
+            'mata_kuliah' => Silabus_Matkul::all(),            
+            'mk_prasyarat' => Silabus_Matkul_Prasyarat::all(),
+            'atribut_softskill' => Silabus_Atribut_Softskill::all(),
+            'media_pembelajaran' => Silabus_Media_Pembelajaran::all(),            
+            'metode_pembelajaran' => Silabus_Sistem_Pembelajaran::all(),                
+            'status_team_teaching' => Status_Team_Teaching::all()
         ];
 
         // Memanggil tampilan form create
--- resources/views/vendor/adminlte/layouts/partials/scripts.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/scripts.blade.php
@@ -1,21 +1,14 @@
-<!-- REQUIRED JS SCRIPTS -->
-<!-- REQUIRED JS SCRIPTS -->
+<script src="{{ asset('/js/app.js') }}" type="text/javascript"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/locale/bootstrap-table-zh-CN.min.js"></script>
+<script src="{{ asset('/js/plugins/timepicker/bootstrap-timepicker.min.js') }}"></script>
 
-<link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.css" rel="stylesheet">
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"></script>
-  <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script>
-<!-- JQuery and bootstrap are required by Laravel 5.3 in resources/assets/js/bootstrap.js-->
-<!-- Laravel App -->
-
-<script src="{{asset('plugins/jquery.dataTables.js')}}" type="text/javascript"></script>
-<script src="{{asset('plugins/dataTables.bootstrap.js')}}" type="text/javascript"></script>
-<link href="{{ asset('/css/bootstrap-tagsinput.css') }}" rel="stylesheet">
-<script type="text/javascript" src="{{ asset('/js/bootstrap-tagsinput.js') }}"></script>
-<script src="{{ mix('/js/app.js') }}" type="text/javascript"></script>
+<script src="http://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js" type="text/javascript"></script>
 <script>
     window.Laravel = {!! json_encode([
         'csrfToken' => csrf_token(),
     ]) !!};
 </script>
+
 @yield('code-footer')
