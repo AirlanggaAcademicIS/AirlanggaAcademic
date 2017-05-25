1998be974b512e7bc0f0dd94027eb8b07373a8b3
azisfaisal29 <gamerscraft@gmail.com>
2017-05-25 20:17:23 +0700

    Create and Delete MKDiambil oleh 081411631029

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::layouts.app')
+@section('code-header')
+
+@endsection
+
+@section('htmlheader_title')
+Dashboard
+@endsection
+
+@section('contentheader_title')
+Dashboard
+@endsection
+
+@section('main-content')
+ 
+@endsection
+@section('code-footer')
+<link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.css" rel="stylesheet">
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script> 
+
+@endsection
