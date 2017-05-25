4120923a69c82e23c142231c41b0f3b1ce88994c
windrasyad <winrasyad@gmail.com>
2017-05-25 22:21:32 +0700

    kegiatan perubahan fitur

--- app/Dokumentasi.php
+++ app/Dokumentasi.php
@@ -7,11 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Dokumentasi extends Model
 {
 
-   protected $table = 'pengajuan_kegiatan';
-   protected $primaryKey = 'id_kegiatan';    
+   protected $table = 'dokumentasi';
+   protected $primaryKey = 'id_dokumentasi';    
    protected $fillable = [
 		'kegiatan_id',
 		'lesson_learned',
 		'url_foto',
       ];
+
+      public function namaKegiatan(){
+      	return $this->belongsTo('App\PengajuanKegiatan','kegiatan_id');
+      }
+
+      public function rondown(){
+         return $this->belongsTo('App\RincianRundown','kegiatan_id');
+      }
 }
