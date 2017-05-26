755ab56e3ca3b722259d3149a77c1e620b7a9a73
ryanrahmanda <gunners.cr7@gmail.com>
2017-05-26 07:00:40 +0700

    Add GUI Tambah Rapat dan Undang Dosen

--- app/NotulensiKaryawan.php
+++ app/NotulensiKaryawan.php
@@ -9,10 +9,10 @@ class NotulensiKaryawan extends Model
    protected $table = 'notulen_rapat';    
    protected $primaryKey = 'id_notulen';    
    protected $fillable = [
-   		'id_notulen',
-   		'permohonan_ruang_id',
-   		'nip_petugas_id',
-   		'nip_id',
+   	'id_notulen',
+   	'permohonan_ruang_id',
+   	'nip_petugas_id',
+   	'nip_id',
 		'nama_rapat',
 		'agenda_rapat',
 		'waktu_pelaksanaan',
