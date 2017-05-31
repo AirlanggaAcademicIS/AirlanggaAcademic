e0176de769951ffba73e6bb47bfda76f26da12ec
Kafifahziyani <ziyanikafifah@gmail.com>
2017-05-31 16:25:43 +0700

    view prestasi

---
+++ resources/views/sidebar_mahasiswa.blade.php
@@ -0,0 +1,24 @@
+<li
+@if($page == 'biodata-mahasiswa')
+{!! 'class="active"'!!}
+@endif
+>
+<a href="{{ url('mahasiswa/biodata-mahasiswa') }}"><i class="fa fa-calculator" aria-hidden="true"></i>Biodata Mahasiswa</a>
+</li>
+<li
+@if($page == 'penelitian')
+{!! 'class="active"'!!}
+@endif
+>
+<!-- Href menuju ke url mahasiswa/biodata -->
+<a href="{{ url('/mahasiswa/penelitian') }}"><i class='fa fa-book'></i>Penelitian</a>
+</li>
+<li
+@if($page == 'prestasi')
+{!! 'class="active"'!!}
+@endif
+>
+<a href="{{ url('mahasiswa/prestasi') }}"><i class="fa fa-calculator" aria-hidden="true"></i>Prestasi</a>
+</li>
+
+	
