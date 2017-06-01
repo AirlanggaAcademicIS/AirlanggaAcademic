56f1e3fd731787792f2be456f40af32ae09da0f1
dianrmdn <dianrmdn1904@gmail.com>
2017-06-01 13:45:14 +0700

    resolve conflict

--- app/Http/Controllers/Dosen/JurnalController.php
+++ app/Http/Controllers/Dosen/JurnalController.php
@@ -28,7 +28,7 @@ class JurnalController extends Controller
             // Memanggil semua isi dari tabel biodata
             'jurnal' => DB::table('jurnal_dosen')
             ->where('jurnal_dosen.nip','=',$dosen)
-            ->join('jurnal','jurnal_dosen.jurnal_id','=','jurnal.jurnal_id')
+            ->join('jurnal','jurnal.jurnal_id','=','jurnal_dosen.jurnal_id')
             ->select('=')
             ->get()
         ];
