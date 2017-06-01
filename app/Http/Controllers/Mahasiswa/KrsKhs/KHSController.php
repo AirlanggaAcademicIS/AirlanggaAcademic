9d5be1607fc5eb3abb14b19a2a97b94c70fc5b59
pandupatra <pandamon56@gmail.com>
2017-06-01 19:19:15 +0700

    Tampilan form cetak khs

--- app/Http/Controllers/Mahasiswa/KrsKhs/KHSController.php
+++ app/Http/Controllers/Mahasiswa/KrsKhs/KHSController.php
@@ -68,11 +68,17 @@ class KHSController extends Controller
     public function toPdf()
     {
        $nim_id = Auth::user()->username;
+       $sum     = DB::table('mk_diambil')
+                ->join('mata_kuliah','mata_kuliah.id_mk','=','mk_diambil.mk_ditawarkan_id')
+                ->select('*')
+                ->where('mk_diambil.mhs_id','=',$nim_id)
+                ->sum('sks'); 
         $data = [
         'page' => 'khs',
         'khs' => KHS::where('mhs_id','=',$nim_id)->get(),
         'tahun' => TahunAkademik::all(),
         'biodata_mhs' => BiodataMahasiswa::where('nim_id','=',$nim_id)->first(),
+        'sum' => $sum,
         
             // 'matkul' => MataKuliah::find($id),
             // 'jenis_matkul' =>JenisMataKuliah::all()
--- resources/views/mahasiswa/krs-khs/khs/cetak.blade.php
+++ resources/views/mahasiswa/krs-khs/khs/cetak.blade.php
@@ -6,41 +6,46 @@
     </title>
 </head>
 <body>
-<img src="http://4.bp.blogspot.com/-q9CDx5hZ_kQ/Vn8eWBxpssI/AAAAAAAAEPk/_Y7Bi4C0YT0/s1600/logo-unair-universitas-airlangga-kecil.png" alt="logo" style="float:left;width:120px;height:120px;">
+<img src="http://4.bp.blogspot.com/-q9CDx5hZ_kQ/Vn8eWBxpssI/AAAAAAAAEPk/_Y7Bi4C0YT0/s1600/logo-unair-universitas-airlangga-kecil.png" alt="logo" style="float:left;width:120px;height:120px;"> &nbsp; 
 <strong>    UNIVERSITAS AIRLANGGA</strong>
-<br>    FAKULTAS SAINS DAN TEKNOLOGI
-<br>    S1-SISTEM INFORMASI
-<br>    KAMPUS C UNAIR, MULYOREJO SURABAYA, 60115
-<br>    Telp. 031-5936501, Fax. 031-5936502
-<br>    https://fst.unair.ac.id, fsaintek@unair.ac.id
+<br> &nbsp;    FAKULTAS SAINS DAN TEKNOLOGI
+<br> &nbsp;   S1-SISTEM INFORMASI
+<br> &nbsp;   KAMPUS C UNAIR, MULYOREJO SURABAYA, 60115
+<br> &nbsp;   Telp. 031-5936501, Fax. 031-5936502
+<br> &nbsp;  https://fst.unair.ac.id, fsaintek@unair.ac.id
 <br>___________________________________________________________________________________________________________
 <b></b>
 <p>
 @foreach($tahun as $i=>$thn)
-<center><b><u>KARTU HASIL STUDI TAHUN AKADEMIK {{$thn->semester_periode}} </u></center></b>
+<center><u>KARTU HASIL STUDI TAHUN AKADEMIK {{$thn->semester_periode}} </u></center>
 <br>
 @endforeach
 </p>
-<br><b>NIM              :   {{$biodata_mhs->nim_id}} </b>
-<br><b>Nama Mahasiswa   :   {{$biodata_mhs->nama_mhs}} </b>
-<table style="width:100%; border: 1px solid black;">
-    <thead>
-        <th style="text-align:center; border: 1px solid black">NO.</th>
-        <th style="text-align:center; border: 1px solid black">NAMA MATA AJAR</th> 
-        <th style="text-align:center; border: 1px solid black">SKS</th>
-        <th style="text-align:center; border: 1px solid black">NILAI</th>
+<br>NIM &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; : &nbsp;   {{$biodata_mhs->nim_id}} 
+<br>Nama Mahasiswa &nbsp; &nbsp; &nbsp;:  &nbsp;  {{$biodata_mhs->nama_mhs}}
+<p></p>
+<table style="width:100%; border: 1px solid black; border-collapse: collapse; font-family: arial ">
+
+    <thead style="background-color: #000000">
+        <th style="text-align:center; color: #ffffff ">NO.</th>
+        <th style="text-align:center; color: #ffffff ">NAMA MATA AJAR</th>
+        <th style="text-align:center; color: #ffffff ">SKS</th>
+        <th style="text-align:center; color: #ffffff ">NILAI</th>
     </thead>
     <tbody>
     @foreach($khs as $i => $k)
-    <tr>
-        <td style="text-align:center; border: 1px solid black">{{$i+1}}</td>
-        <td style="text-align:center; border: 1px solid black">{{$k->MKDitawarkan->MK->nama_matkul}}</td>
-        <td style="text-align:center; border: 1px solid black">{{$k->MKDitawarkan->MK->sks}}</td>
-        <td style="text-align:center; border: 1px solid black">{{$k->nilai}}</td> 
+    <tr style="border: 1px solid black;">
+        <td style="text-align:center; border: 1px solid black;">{{$i+1}}</td>
+        <td style="text-align:center; border: 1px solid black;">{{$k->MKDitawarkan->MK->nama_matkul}}</td>
+        <td style="text-align:center; border: 1px solid black;">{{$k->MKDitawarkan->MK->sks}}</td>
+        <td style="text-align:center; border: 1px solid black;">{{$k->nilai}}</td> 
         </tr>
         @endforeach
     </tbody>
 </table>
+<br>
+<br>Tanpa mata ajar dengan nilai E, hasil studi sampai semester ini adalah :
+<br>JUMLAH SKS YANG TELAH DITEMPUH = {{$sum}} , dengan IPK = 
 
 </body>
 </html>
