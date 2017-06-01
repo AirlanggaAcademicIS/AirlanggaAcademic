8e84980b9a5b747eb1f37b8f702145bd019377e4
rizaldifirdaus <rizaldilucu@gmail.com>
2017-06-01 17:07:05 +0700

    crud matkul, crd matkul prodi,  crud kategori capaian pembelajaran

--- storage/framework/views/ca0c7f8a2d54c19838bf542d58312ecb0f395802.php
+++ storage/framework/views/ca0c7f8a2d54c19838bf542d58312ecb0f395802.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 <script src="<?php echo e(asset('/js/app.js')); ?>" type="text/javascript"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.js"></script>
@@ -8,7 +6,6 @@
 
 <script src="http://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js" type="text/javascript"></script>
 
->>>>>>> 56f1e3fd731787792f2be456f40af32ae09da0f1
 <!-- Laravel App -->
 <script src="<?php echo e(mix('/js/app.js')); ?>" type="text/javascript"></script>
 <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"></script>
@@ -45,15 +42,7 @@
 <script src="http://code.jquery.com/ui/1.11.0/jquery-ui.js"></script>
 <script src="<?php echo e(asset('/plugins/bootstrap-datepicker.js')); ?>"></script>
 <script src="<?php echo e(asset('/js/app.js')); ?>" type="text/javascript"></script>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 8a5d36fae8e494c1c6fccc7b89a4b8f0c213a374
-=======
-
->>>>>>> 56f1e3fd731787792f2be456f40af32ae09da0f1
 <script src="<?php echo e(asset('/js/jquery.steps.js')); ?>" type="text/javascript"></script>
 <script src="http://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js" type="text/javascript"></script>
 <script src="<?php echo e(asset('/js/jquery-ui.js')); ?>" type="text/javascript"></script>
@@ -68,10 +57,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.6/fullcalendar.min.js"></script>
 
-<<<<<<< HEAD
-=======
 
->>>>>>> cb3a990c9b595fc9e243e57aa0471124ac1e97e7
 <script src="<?php echo e(asset('/js/app.js')); ?>" type="text/javascript"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.js"></script>
@@ -79,23 +65,13 @@
 <script src="<?php echo e(asset('/js/plugins/timepicker/bootstrap-timepicker.min.js')); ?>"></script>
 
 <script src="http://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js" type="text/javascript"></script>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
->>>>>>> 8a5d36fae8e494c1c6fccc7b89a4b8f0c213a374
-=======
-
->>>>>>> 56f1e3fd731787792f2be456f40af32ae09da0f1
 <script>
     window.Laravel = <?php echo json_encode([
         'csrfToken' => csrf_token(),
     ]); ?>;
 </script>
-<<<<<<< HEAD
-=======
 
->>>>>>> cb3a990c9b595fc9e243e57aa0471124ac1e97e7
 <!-- fullCalendar 2.2.5 -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.6/fullcalendar.min.js"></script>
@@ -121,34 +97,9 @@
             {
                 var form = $(this);
                 // Submit form input
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
-                form.submit();
-            }           
-    });
-
-    </script>
-<?php echo $__env->yieldContent('code-footer'); ?>
-
-
-
-                form.submit();
-            }           
-    });
-
-=======
-                form.submit();
-            }           
-    });
->>>>>>> 8a5d36fae8e494c1c6fccc7b89a4b8f0c213a374
-    </script>
-=======
                 form.submit();
             }           
     });
 </script>
->>>>>>> 56f1e3fd731787792f2be456f40af32ae09da0f1
 
 <?php echo $__env->yieldContent('code-footer'); ?>
