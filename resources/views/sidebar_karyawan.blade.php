b9f8f422067059973b92f4f7ecc68b2c932e5726
rizqiromdloni <rizqi.romdloni@gmail.com>
2017-06-02 09:14:36 +0700

    Perbaikan Popup Modal

--- resources/views/sidebar_karyawan.blade.php
+++ resources/views/sidebar_karyawan.blade.php
@@ -4,25 +4,21 @@
 {!! 'class="active"'!!}
 @endif
 >
-
 <a href="{{ url('transaksi') }}"><i class="fa fa-calculator" aria-hidden="true"></i>Transaksi</a>
 </li> -->
+<li>
+            <a href=""><i class='fa fa-users'></i> <span> Notulensi</span></a>
+            <ul class="treeview-menu">
+            <!-- Sidebarnya ditaruh dibawah sini -->
 
-
-<li
-@if($page == 'AkunMahasiswa')
-{!! 'class="active"'!!}
-@endif
->
-<a href="{{ url('karyawan/akun') }}"><i class="fa fa-calculator" aria-hidden="true"></i>Akun Mahasiswa</a>
-</li>
-<li
-@if($page == 'verifikasi')
-{!! 'class="active"'!!}
-@endif
->
-<a href="{{ url('karyawan/verifikasi') }}"><i class="fa fa-book" aria-hidden="true"></i>Verifikasi Mahasiswa</a>
-</li>
-
-</ul>
-</li>
+            <li
+            @if($page == 'notulensi')
+            {!! 'class="active"'!!}
+            @endif
+            >
+            <!-- Href menuju ke url notulensi/notulensi rapat -->
+            <a href="#"><i class='fa fa-book'></i> <span>Undangan</span></a>
+            <a href="{{url('notulensi')}}"><i class='fa fa-book'></i> <span>Notulensi</span></a>
+                 <!--  <a href="#"><i class='fa fa-book'></i> <span>Kehadiran Rapat</span></a> -->
+            <a href="#"><i class='fa fa-book'></i> <span>Kalender</span></a>
+            </li>
