ff839db6a788db83c35606b40ff93ba697fde639
pratomoadi <tommyatmaji@gmail.com>
2017-06-07 09:39:59 +0700

    memperbaiki konfirmasi kegiatan dan daftar konfirmasi untuk karyawan

--- app/Http/Controllers/Dosen/pengelolaankegiatan/DetailPengajuanController.php
+++ app/Http/Controllers/Dosen/pengelolaankegiatan/DetailPengajuanController.php
@@ -15,6 +15,14 @@ use App\RincianDana;
 use App\RincianRundown;
 use App\Dokumentasi;
 use App\KategoriDana;
+use App\StrukturPanitia;
+use App\BiodataMahasiswa;
+use App\MahasiswaPanitia;
+use App\MahasiswaPengajuan;
+use App\StrukturPanitiaDosen;
+use App\DosenPanitia;
+use App\DosenPengajuan;
+use App\JabatanPanitia;
 
 class DetailPengajuanController extends Controller
 {
@@ -22,6 +30,8 @@ class DetailPengajuanController extends Controller
     // Function untuk menampilkan tabel
     public function index($id_kegiatan)
     {
+            $struktur = StrukturPanitia::where('kegiatan_id',$id_kegiatan)->count();
+            if($struktur>0){
             $data = [
             // Buat di sidebar, biar ketika diklik yg aktif sidebar biodata
             'page' => 'konfirmasi-kegiatan',
@@ -32,11 +42,32 @@ class DetailPengajuanController extends Controller
             'rundownProposal' => RincianRundown::where('kegiatan_id',$id_kegiatan)->where('kategori_rundown','0')->get(),
             'danaProposal' => RincianDana::where('kegiatan_id',$id_kegiatan)->where('kategori_dana','0')->get(),
             'rundownLPJ' => RincianRundown::where('kegiatan_id',$id_kegiatan)->where('kategori_rundown','1')->get(),
-            'danaLPJ' => RincianDana::where('kegiatan_id',$id_kegiatan)->where('kategori_dana','1')->get()
+            'danaLPJ' => RincianDana::where('kegiatan_id',$id_kegiatan)->where('kategori_dana','1')->get(),
+            'struktur' => StrukturPanitia::where('kegiatan_id',$id_kegiatan)->get(),
+            'jabatan' => JabatanPanitia::all()
         ];
+        return view('karyawan.pengelolaan-kegiatan.detail-pengajuan.index',$data);
+            }
+
+            else{
+                $data = [
+            // Buat di sidebar, biar ketika diklik yg aktif sidebar biodata
+            'page' => 'konfirmasi-kegiatan',
+            // Memanggil semua isi dari tabel biodata
+
+            'konfirmasiKegiatan' => PengajuanKegiatan::where('id_kegiatan',$id_kegiatan)->get(),
+            'lpj' => Dokumentasi::where('kegiatan_id',$id_kegiatan)->get(),
+            'rundownProposal' => RincianRundown::where('kegiatan_id',$id_kegiatan)->where('kategori_rundown','0')->get(),
+            'danaProposal' => RincianDana::where('kegiatan_id',$id_kegiatan)->where('kategori_dana','0')->get(),
+            'rundownLPJ' => RincianRundown::where('kegiatan_id',$id_kegiatan)->where('kategori_rundown','1')->get(),
+            'danaLPJ' => RincianDana::where('kegiatan_id',$id_kegiatan)->where('kategori_dana','1')->get(),
+            'struktur' => StrukturPanitiaDosen::where('kegiatan_id',$id_kegiatan)->get(),
+            'jabatan' => JabatanPanitia::all()
+        ];
+        return view('karyawan.pengelolaan-kegiatan.detail-pengajuan.indexLPJ',$data);
+            }
 
         // Memanggil tampilan index di folder mahasiswa/biodata dan juga menambahkan $data tadi di view
-        return view('dosen.pengelolaan-kegiatan.detail-pengajuan.index',$data);
     }
 
     
