9595fb7f7b6587e0570898f71924c9a2f1632e00
dionisiusnanda <dionisiusn@yahoo.com>
2017-06-08 06:12:49 +0700

    Modul Kegiatan, fitur view detail dan status

--- resources/views/dosen/pengelolaan-kegiatan/status/ditolak.blade.php
+++ resources/views/dosen/pengelolaan-kegiatan/status/ditolak.blade.php
@@ -15,7 +15,7 @@ Daftar Kegiatan Akademik
 
 @section('main-content')
 <!-- include summernote css/js-->
-<h2>Proposal Kegiatan Akademik Sedang Diajukan
+<h2>Revisi Proposal Kegiatan Akademik
 </h2>
 
 
@@ -27,6 +27,7 @@ Daftar Kegiatan Akademik
       <th style="text-align:center">No.</th>
       <th style="text-align:center">Nama Kegiatan</th>
       <th style="text-align:center">Tanggal Pengajuan</th>
+      <th style="text-align:center">Revisi</th>
       <th style="text-align:center">Poster</th>
       <th style="text-align:center">Action</th>
 
@@ -38,17 +39,16 @@ Daftar Kegiatan Akademik
       <td width="10%" style="text-align:center">{{ $i+1 }}</td>
       <td width="15%" style="text-align:center">{{$pen->nama}}</td>
       <td width="10%" style="text-align:center">{{$pen->tglpengajuan}}</td>
-      <td width="45%" style="text-align:center"><img src="{{URL::asset('/img/pengajuan/'.$pen->url_poster)}}" height="100px" width="100px" hspace="5px" vspace="2px"></td>
+      <td width="35%" style="text-align:center">{{$pen->revisi}}</td>
+      <td width="20%" style="text-align:center"><img src="{{URL::asset('/img/pengajuan/'.$pen->url_poster)}}" height="100px" width="100px" hspace="5px" vspace="2px"></td>
       <td width="20%" style="text-align:center">
-      @if($pen->konfirmasi_proposal==2)
-      <a href="{{url('/mahasiswa/pengelolaan-kegiatan/detail-pengajuan/'.$pen->id_kegiatan)}}" class="btn btn-success btn-xs"> Revisi</a>
-      @endif
-        <a href="{{url('/mahasiswa/pengelolaan-kegiatan/detail-pengajuan/'.$pen->id_kegiatan)}}" class="btn btn-success btn-xs"> View Detail</a>
+      <a href="{{url('/dosen/pengelolaan-kegiatan/pengajuan/'.$pen->id_kegiatan.'/revisi')}}" class="btn btn-danger btn-xs"> Revisi</a>
+        <a href="{{url('/dosen/pengelolaan-kegiatan/detail-pengajuan/'.$pen->id_kegiatan)}}" class="btn btn-success btn-xs"> View Detail</a>
         </td>
     </tr>
      @empty
         <tr>
-          <td colspan="6"><center>Belum ada Pengajuan Proposal Kegiatan</center></td>
+          <td colspan="6"><center>Belum ada Revisi Proposal Kegiatan Akademik</center></td>
         </tr>
     @endforelse
   </tbody>
@@ -56,7 +56,7 @@ Daftar Kegiatan Akademik
 </div>
 
 
-<h2>LPJ Kegiatan Akademik Sedang Diajukan
+<h2>Revisi LPJ Kegiatan Akademik
 </h2>
 <div style="overflow: auto">
 <table id="myTable" class="table table-striped table-bordered" cellspacing="0">
@@ -65,6 +65,7 @@ Daftar Kegiatan Akademik
       <th style="text-align:center">No.</th>
       <th style="text-align:center">Nama Kegiatan</th>
       <th style="text-align:center">Tanggal Pelaksanaan</th>
+      <th style="text-align:center">Revisi</th>
       <th style="text-align:center">Poster</th>
       <th style="text-align:center">Action</th>
 
@@ -76,19 +77,16 @@ Daftar Kegiatan Akademik
       <td width="10%" style="text-align:center">{{ $i+1 }}</td>
       <td width="15%" style="text-align:center">{{$pen->nama}}</td>
       <td width="10%" style="text-align:center">{{$pen->tglpelaksanaan}}</td>
+      <td width="10%" style="text-align:center">{{$pen->revisi}}</td>
       <td width="45%" style="text-align:center"><img src="{{URL::asset('/img/pengajuan/'.$pen->url_poster)}}" height="100px" width="100px" hspace="5px" vspace="2px"></td>
       <td width="20%" style="text-align:center">
-      @if($pen->konfirmasi_proposal==1)
-      @if($pen->konfirmasi_lpj==3)
-      <a href="{{url('/mahasiswa/pengelolaan-kegiatan/detail-pengajuan/'.$pen->id_kegiatan)}}" class="btn btn-success btn-xs"> Revisi</a>
-      @endif
-      @endif
-        <a href="{{url('/mahasiswa/pengelolaan-kegiatan/detail-pengajuan/'.$pen->id_kegiatan)}}" class="btn btn-success btn-xs"> View Detail</a>
+      <a href="{{url('/dosen/pengelolaan-kegiatan/dokumentasi/'.$pen->id_kegiatan.'/revisiLPJ')}}" class="btn btn-success btn-xs"> Revisi</a>
+        <a href="{{url('/dosen/pengelolaan-kegiatan/detail-pengajuan/'.$pen->id_kegiatan)}}" class="btn btn-success btn-xs"> View Detail</a>
         </td>
     </tr>
      @empty
         <tr>
-          <td colspan="6"><center>Belum ada Pengajuan LPJ Kegiatan</center></td>
+          <td colspan="6"><center>Belum ada Revisi LPJ Kegiatan Akademik</center></td>
         </tr>
     @endforelse
   </tbody>
--- resources/views/mahasiswa/pengelolaan-kegiatan/status/dikonfirmasi.blade.php
+++ resources/views/mahasiswa/pengelolaan-kegiatan/status/dikonfirmasi.blade.php
@@ -15,29 +15,62 @@ Daftar Kegiatan Akademik
 
 @section('main-content')
 <!-- include summernote css/js-->
-<h2>Proposal Kegiatan Akademik Sedang Diajukan
-</h2>
+@foreach($Status as $i)
+@if($i->konfirmasi_proposal==1)
+@if($i->konfirmasi_lpj==0)
+<h2>Kegiatan Akademik Yang Akan Datang </h2>
+@endif
+@endif
 
+@if($i->konfirmasi_proposal==1)
+@if($i->konfirmasi_lpj==2)
+<h2>Kegiatan Akademik Terlaksana
+</h2>
+@endif
+@endif
 
 <!--untuk pengajuan sedang diproses-->
 <div style="overflow: auto">
 <table id="myTable" class="table table-striped table-bordered" cellspacing="0">
   <thead>
+   @foreach($Status as $i => $pen) 
     <tr>
       <th style="text-align:center">No.</th>
       <th style="text-align:center">Nama Kegiatan</th>
+      @if($pen->konfirmasi_proposal==1)
+      @if($pen->konfirmasi_lpj==0)
       <th style="text-align:center">Tanggal Pengajuan</th>
+      @endif
+      @endif
+
+      @if($pen->konfirmasi_proposal==1)
+      @if($pen->konfirmasi_lpj==2)
+      <th style="text-align:center">Tanggal Pelaksanaan</th>
+      @endif
+      @endif
+
       <th style="text-align:center">Poster</th>
       <th style="text-align:center">Action</th>
 
     </tr>
+    @endforeach
     </thead>
   <tbody>
    @forelse($Status as $i => $pen) 
     <tr>
       <td width="10%" style="text-align:center">{{ $i+1 }}</td>
       <td width="15%" style="text-align:center">{{$pen->nama}}</td>
+      @if($pen->konfirmasi_proposal==1)
+      @if($pen->konfirmasi_lpj==0)
       <td width="10%" style="text-align:center">{{$pen->tglpengajuan}}</td>
+      @endif
+      @endif
+
+      @if($pen->konfirmasi_proposal==1)
+      @if($pen->konfirmasi_lpj==2)
+      <td width="10%" style="text-align:center">{{$pen->tglpelaksanaan}}</td>
+      @endif
+      @endif
       <td width="45%" style="text-align:center"><img src="{{URL::asset('/img/pengajuan/'.$pen->url_poster)}}" height="100px" width="100px" hspace="5px" vspace="2px"></td>
       <td width="20%" style="text-align:center">
       @if($pen->konfirmasi_proposal==2)
@@ -48,52 +81,14 @@ Daftar Kegiatan Akademik
     </tr>
      @empty
         <tr>
-          <td colspan="6"><center>Belum ada Pengajuan Proposal Kegiatan</center></td>
+          <td colspan="6"><center>Belum ada Pengajuan Kegiatan</center></td>
         </tr>
     @endforelse
   </tbody>
 </table>
 </div>
 
-
-<h2>LPJ Kegiatan Akademik Sedang Diajukan
-</h2>
-<div style="overflow: auto">
-<table id="myTable" class="table table-striped table-bordered" cellspacing="0">
-  <thead>
-    <tr>
-      <th style="text-align:center">No.</th>
-      <th style="text-align:center">Nama Kegiatan</th>
-      <th style="text-align:center">Tanggal Pelaksanaan</th>
-      <th style="text-align:center">Poster</th>
-      <th style="text-align:center">Action</th>
-
-    </tr>
-    </thead>
-  <tbody>
-   @forelse($StatusLPJ as $i => $pen) 
-    <tr>
-      <td width="10%" style="text-align:center">{{ $i+1 }}</td>
-      <td width="15%" style="text-align:center">{{$pen->nama}}</td>
-      <td width="10%" style="text-align:center">{{$pen->tglpelaksanaan}}</td>
-      <td width="45%" style="text-align:center"><img src="{{URL::asset('/img/pengajuan/'.$pen->url_poster)}}" height="100px" width="100px" hspace="5px" vspace="2px"></td>
-      <td width="20%" style="text-align:center">
-      @if($pen->konfirmasi_proposal==1)
-      @if($pen->konfirmasi_lpj==3)
-      <a href="{{url('/mahasiswa/pengelolaan-kegiatan/detail-pengajuan/'.$pen->id_kegiatan)}}" class="btn btn-success btn-xs"> Revisi</a>
-      @endif
-      @endif
-        <a href="{{url('/mahasiswa/pengelolaan-kegiatan/detail-pengajuan/'.$pen->id_kegiatan)}}" class="btn btn-success btn-xs"> View Detail</a>
-        </td>
-    </tr>
-     @empty
-        <tr>
-          <td colspan="6"><center>Belum ada Pengajuan LPJ Kegiatan</center></td>
-        </tr>
-    @endforelse
-  </tbody>
-</table>
-</div>
+@endforeach
 @endsection
 
 @section('code-footer')
