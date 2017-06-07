6283a8b9251dd2bffc972509efb9794d8ffea0f8
windrasyad <winrasyad@gmail.com>
2017-06-07 23:24:17 +0700

    integrasi kegiatan

--- app/Http/Controllers/Mahasiswa/DokumentasiController.php
+++ app/Http/Controllers/Mahasiswa/DokumentasiController.php
@@ -1,6 +1,7 @@
 <?php 
 
-namespace App\Http\Controllers\Mahasiswa;
+
+namespace App\Http\Controllers\Mahasiswa\pengelolaankegiatan;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
@@ -14,22 +15,41 @@ use Response;
 use App\Dokumentasi;
 use App\PengajuanKegiatan;
 use App\RincianRundown;
+use App\StrukturPanitia;
+use Auth;
 use Illuminate\Support\Facades\DB;
 use PDF;
 
 class DokumentasiController extends Controller
 {
-
     // Function untuk menampilkan tabel
     public function index()
     {
-        $data = [
-            // Buat di sidebar, biar ketika diklik yg aktif sidebar biodata
-            'page' => 'dokumentasi',
-            // Memanggil semua isi dari tabel biodata
-            'dokumentasi' => Dokumentasi::all()
+        // $data = [
+        //     // Buat di sidebar, biar ketika diklik yg aktif sidebar biodata
+        //     'page' => 'dokumentasi',
+        //     // Memanggil semua isi dari tabel biodata
+        //     'dokumentasi' => Dokumentasi::all()
+        //     'kegiatan' => PengajuanKegiatan::where('konfirmasi_proposal','1')->where('konfirmasi_lpj','0')->get()
 
         
+        // ];
+        $nim = Auth::user()->username;
+        $data = [
+            // Buat di sidebar, biar ketika diklik yg aktif sidebar biodata
+            'page' => 'Status',
+            // Memanggil semua isi dari tabel biodata
+            'Status' => DB::table('mhs_kegiatan')
+            ->join('pengajuan_kegiatan','pengajuan_kegiatan.id_kegiatan' , '=', 'mhs_kegiatan.kegiatan_id') 
+            ->join('biodata_mhs', 'biodata_mhs.nim_id', '=', 'mhs_kegiatan.nim_id')
+            // ->join('dokumentasi', 'dokumentasi.kegiatan_id', '=', 'mhs_kegiatan.kegiatan_id') 
+            ->select('*')
+            ->where('mhs_kegiatan.nim_id', '=', $nim)
+            ->where('pengajuan_kegiatan.konfirmasi_proposal','=','1')
+            ->where('pengajuan_kegiatan.konfirmasi_lpj','=','0')
+            ->whereNull('pengajuan_kegiatan.deleted_at')
+            ->get(),
+
         ];
         // Memanggil tampilan index di folder mahasiswa/biodata dan juga menambahkan $data tadi di view
         return view('mahasiswa.pengelolaan-kegiatan.dokumentasi.index',$data);
@@ -37,18 +57,28 @@ class DokumentasiController extends Controller
 
     public function create()
     {
+       
+        $nim = Auth::user()->username;
         $data = [
-            // Buat di sidebar, biar ketika diklik yg aktif sidebar Biodata
-            'page' => 'dokumentasi',
-            'dokumentasi'=> array(),
-            'kegiatan' => PengajuanKegiatan::all()
-        ];
+            // Buat di sidebar, biar ketika diklik yg aktif sidebar biodata
+            'page' => 'Status',
+            // Memanggil semua isi dari tabel biodata
+            'Status' => DB::table('mhs_kegiatan')
+            ->join('pengajuan_kegiatan','pengajuan_kegiatan.id_kegiatan' , '=', 'mhs_kegiatan.kegiatan_id') 
+            ->join('biodata_mhs', 'biodata_mhs.nim_id', '=', 'mhs_kegiatan.nim_id') 
+            ->select('*')
+            ->where('mhs_kegiatan.nim_id', '=', $nim)
+            ->where('pengajuan_kegiatan.konfirmasi_proposal','=','1')
+            ->where('pengajuan_kegiatan.konfirmasi_lpj','=','0')
+            ->whereNull('pengajuan_kegiatan.deleted_at')
+            ->get(),
 
+            ];
         // Memanggil tampilan form create
-    	return view('mahasiswa.pengelolaan-kegiatan.dokumentasi.create',$data);
+        return view('mahasiswa.pengelolaan-kegiatan.dokumentasi.create',$data);
     }
 
-    public function selected($id,Request $request)
+    public function selected($id, Request $request)
     {
         $data = [
             // Buat di sidebar, biar ketika diklik yg aktif sidebar Biodata
@@ -86,18 +116,18 @@ class DokumentasiController extends Controller
         // Menginsertkan apa yang ada di form ke dalam tabel biodata
         Dokumentasi::create($dok);
             $gambar = $request->file('url_foto')->move("img/dokumentasi/",$dok['url_foto']);
-
-        $pk = PengajuanKegiatan::find($request->input('kegiatan_id'));
-        $pk->rpelaksanaan = $request->input('rpelaksanaan');
-        $pk->konfirmasi_proposal = '1';
-        $pk->konfirmasi_lpj = '1';
-        $pk->save();
-
+        $id = $request->input('kegiatan_id');
         // Menampilkan notifikasi pesan sukses
+
+        $kegiatan = PengajuanKegiatan::find($id);
+        $kegiatan->konfirmasi_lpj = '1';
+        $kegiatan->tglpelaksanaan = $request->input('tglpelaksanaan');
+        $kegiatan->rpelaksanaan = $request->input('rpelaksanaan');
+        $kegiatan->save();
         Session::put('alert-success', 'Dokumentasi berhasil ditambahkan');
 
         // Kembali ke halaman mahasiswa/biodata
-        return Redirect::to('mahasiswa/dokumentasi');
+        return Redirect::to('mahasiswa/pengelolaan-kegiatan/rincian-rundown/'.$id.'/lpj');
     }
 
     public function delete($id_dokumentasi)
@@ -109,54 +139,71 @@ class DokumentasiController extends Controller
         $dokumentasi->delete();
 
         // Menampilkan notifikasi pesan sukses
-    	Session::put('alert-success', 'gambar berhasil dihapus');
+        Session::put('alert-success', 'gambar berhasil dihapus');
 
         // Kembali ke halaman sebelumnya
-      	return Redirect::back();	 
+        return Redirect::back();     
     }
 
    public function edit($id)
     {
+        $nim = Auth::user()->username;
         $data = [
             // Buat di sidebar, biar ketika diklik yg aktif sidebar biodata
-            'page' => 'dokumentasi',
-            // Mencari biodata berdasarkan id
-            'dokumentasi' => Dokumentasi::find($id),
+            'page' => 'Status',
+            // Memanggil semua isi dari tabel biodata
+            'Status' => DB::table('mhs_kegiatan')
+            ->join('pengajuan_kegiatan','pengajuan_kegiatan.id_kegiatan' , '=', 'mhs_kegiatan.kegiatan_id')
+            ->join('dokumentasi', 'dokumentasi.kegiatan_id','=','mhs_kegiatan.kegiatan_id')
+            ->select('*')
+            ->where('mhs_kegiatan.nim_id', '=', $nim)
+            ->where('mhs_kegiatan.kegiatan_id','=',$id)
+            ->whereNull('pengajuan_kegiatan.deleted_at')
+            ->first(),
 
-            'kegiatan' => PengajuanKegiatan::find($id)
-        ];
-
+            ];
         // Menampilkan form edit dan menambahkan variabel $data ke tampilan tadi, agar nanti value di formnya bisa ke isi
         return view('mahasiswa.pengelolaan-kegiatan.dokumentasi.edit',$data);
     }
 
     public function editAction($id, Request $request)
     {
-        // Mencari biodata yang akan di update dan menaruhnya di variabel $biodata
-        $dokumentasi = Dokumentasi::find($id);
 
-        // Mengupdate $biodata tadi dengan isi dari form edit tadi
-        // $dokumentasi->nama_dokumentasi = $request->input('nama_dokumentasi');
-        $dokumentasi->lesson_learned = $request->input('lesson_learned');
-        // $dokumentasi->gambar = $request->input('url_foto');
-        $dokumentasi->save();
+        $evaluasi = Dokumentasi::where('kegiatan_id',$id)->first();
+         $dok = $request->input();
+        $dok['url_foto']= time() .'.'.$request->file('url_foto')->getClientOriginalExtension();
 
-        // Notifikasi sukses
-        Session::put('alert-success', 'Dokumentasi berhasil diedit');
+        $evaluasi->lesson_learned = $request->input('lesson_learned');
+        $evaluasi->url_foto = $dok['url_foto'];
+        $evaluasi->save();
+
+            $gambar = $request->file('url_foto')->move("img/dokumentasi/",$dok['url_foto']);
+        $id = $request->input('kegiatan_id');
+        // Menampilkan notifikasi pesan sukses
+
+        $kegiatan = PengajuanKegiatan::find($id);
+        $kegiatan->konfirmasi_lpj = '1';
+        $kegiatan->tglpelaksanaan = $request->input('tglpelaksanaan');
+        $kegiatan->rpelaksanaan = $request->input('rpelaksanaan');
+        $kegiatan->save();
+        Session::put('alert-success', 'Dokumentasi berhasil direvisi');
 
         // Kembali ke halaman mahasiswa/biodata
-        return Redirect::to('mahasiswa/dokumentasi');
+        return Redirect::to('mahasiswa/pengelolaan-kegiatan/rincian-rundown/'.$id.'/editLPJ');
     }
 
-    public function toPdf($id_kegiatan)
+     public function toPdf($id_kegiatan)
     {   
         $data = [
             'kegiatan' => PengajuanKegiatan::find($id_kegiatan),
-            'dokumentasi' => Dokumentasi::all()
+            'dokumentasi' => Dokumentasi::where('kegiatan_id','=',$id_kegiatan)->get(),
+            'dana' => RincianDana::where('kegiatan_id','=',$id_kegiatan)->get(),
+            'rundownLPJ' => RincianRundown::where('kegiatan_id','=',$id_kegiatan)->get()
         ];
-
         $pdf = PDF::loadView('mahasiswa.pengelolaan-kegiatan.detail-pengajuan.pdf', $data);
         return $pdf->download('pengelolaan-kegiatan.pdf');
     }
+
+
 
 }
