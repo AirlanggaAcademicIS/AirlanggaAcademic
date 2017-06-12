bd4bb122cd40d485f3cc1a4547686a9fda170999
kretawiweka <ekanuraga@gmail.com>
2017-06-13 00:09:24 +0700

    finisihing rps-silabus [kurikulum]

--- config/snappy.php
+++ config/snappy.php
@@ -5,14 +5,14 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary'  => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"',
+        'binary'  => '/usr/local/bin/wkhtmltopdf',
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
     ),
     'image' => array(
         'enabled' => true,
-        'binary'  => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage.exe"',
+        'binary'  => '/usr/local/bin/wkhtmltoimg',
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
