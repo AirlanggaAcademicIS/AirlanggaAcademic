c820d56f44da8c79f8561306149e3b092e0ff2f1
mhakimni <mhakimnuri@gmail.com>
2017-06-13 01:45:42 +0700

    merapikan view upload file e-learning dosen

--- resources/views/sidebar_mahasiswa.blade.php
+++
@@ -1,6 +0,0 @@
-<<<<<<< HEAD
-=======
-<li>
-<a href="{{ url('/inventaris/mahasiswa/asset') }}"><i class="fa fa-calculator" aria-hidden="true"></i>Inventaris</a>
-</li>
->>>>>>> cf7ed3b6320656f692b5b62fc0b0e4536eb21316
