791cd946191aff5a6ac1f962030235148dea5e46
dikkisetiawan <initialsDS@gmail.com>
2017-06-15 11:46:29 +0700

    view history asset
    menambahkan view history pada asset

--- resources/views/karyawan/inventaris/asset/historyAsset.blade.php
+++ resources/views/karyawan/inventaris/asset/historyAsset.blade.php
@@ -10,7 +10,7 @@ Asset
 @endsection
 
 @section('contentheader_title')
-Asset
+History Asset
 @endsection
 
 @section('main-content')
@@ -26,18 +26,13 @@ Asset
   @endif
   @endforeach
 </div>
-<div class="row"><div class="col-sm-6">
-<div style="margin-bottom: 10px">
-  <!-- Href ini biar diklik masuk ke form tambah -->
-  <a href="{{url('inventaris/asset/create')}}" type="button" class="btn btn-info btn-md" >
-    <i class="fa fa-plus-square"></i> Tambah Asset</a>
-  <a href="{{url('inventaris/asset/location-report')}}" type="button" class="btn btn-info btn-md" >
-    <i class="fa fa-file-pdf-o" ></i> Location Report</a>
-</div>
-</div><div class="col-sm-6">
+
+
+
+<div class="col-sm-6">
 
 </div>
-</div>
+
 
 <div style="overflow: auto">
 <table class="table" id="data-table" style="width:100%">
@@ -49,8 +44,7 @@ Asset
       <th style="text-align:center">Status</th>
       <th style="text-align:center">Nama Asset</th>
       <th style="text-align:center">Lokasi</th> 
-      <th style="text-align:center">Action</th>
-      <th style="text-align:center">Maintenance</th>
+    
       </tr>
     </thead>
   <tbody>
@@ -99,30 +93,7 @@ Asset
         <td width="20%" style="text-align:center">Labkom 6</td>
       @endif
 
-      <td width="20%" style="text-align:center" ><a onclick="return confirm('Anda yakin untuk menghapus asset ini?');" href="{{url('inventaris/asset/'.$ass->id_asset.'/delete/')}}" class="btn btn-danger btn-xs">
-        <i class="fa fa-trash-o"></i> Hapus</a>
-
-        <a href="{{url('inventaris/asset/'.$ass->id_asset.'/viewDetail/')}}" class="btn btn-warning btn-xs">
-        <i class="fa fa-eye"></i> View Detail</a>
-              
-        <a href="{{url('inventaris/asset/'.$ass->id_asset.'/edit/')}}" class="btn btn-success btn-xs">
-        <i class="fa fa-pencil-square-o"></i> Edit</a>
-
-        <a href="{{url('inventaris/input-peminjaman/'.$ass->id_asset.'')}}" class="btn btn-primary btn-xs">
-        <i class="fa fa-hand-rock-o"></i> Pinjam Asset</a>
-
-        <a href="{{url('/inventaris/asset/barcode/'.$ass->id_asset.'')}}" class="btn btn-info btn-xs">
-        <i class="fa fa-qrcode"></i> Get QRCODE</a>
-
-        <a href="{{url('/inventaris/asset/barcode/'.$ass->id_asset.'')}}" class="btn btn-info btn-xs">
-        <i class="fa fa-qrcode"></i> History Asset</a>
-
-        </td>
-        <td width="20%" style="text-align:center" >
-        <a href="{{url('inventaris/maintenance/input-maintenance/'.$ass->id_asset.'')}}" class="btn btn-primary btn-xs">
-        <i class="fa fa-wrench"></i> Maintenance </a>
-
-        </td>
+    
     </tr>
      @empty
         <tr>
