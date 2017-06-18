9fd5d1dc4e52167d652ed1d2cae2ebad485ba7fd
kennykarnama <kennykarnama@gmail.com>
2017-06-18 13:48:07 +0700

    fix logic error controller jadwal sidang dan hasil sidang dan update tampilan jadwal dan hasil sidang

--- app/Http/Controllers/Dosen/monitoringskripsi/JadwalSidangController.php
+++ app/Http/Controllers/Dosen/monitoringskripsi/JadwalSidangController.php
@@ -39,14 +39,16 @@ class JadwalSidangController extends Controller
         $dosen = DB::table('dosen')->get();
         $petugas_tu = DB::table('petugas_tu')->get();
 
+        $data_skripsi = DB::table('skripsi')->get();
+
         $jadwal_sidang_proposal = DB::table('skripsi')
-            ->leftJoin('mahasiswa', 'skripsi.NIM_id', '=', 'mahasiswa.nim')
+             ->leftJoin('biodata_mhs', 'skripsi.NIM_id', '=', 'biodata_mhs.nim_id')
             ->leftJoin('kbk', 'skripsi.kbk_id', '=', 'kbk.id_kbk')
             ->leftJoin('petugas_tu','skripsi.nip_petugas_id','=','petugas_tu.nip_petugas')
             ->leftJoin('ruang','skripsi.tempat_sidangpro','=','ruang.id_ruang')
             ->leftJoin('dosen_penguji','dosen_penguji.skripsi_id','=','skripsi.id_skripsi')
             ->leftjoin('dosen_pembimbing','dosen_pembimbing.skripsi_id','=','skripsi.id_skripsi')
-            ->select('skripsi.id_skripsi','mahasiswa.nim', 'skripsi.NIM_id', 'kbk.jenis_kbk', 'skripsi.Judul', 'skripsi.tgl_sidangpro', 'skripsi.waktu_sidangpro', 'dosen_pembimbing.nip_id as dosbing','ruang.nama_ruang','dosen_penguji.nip_id as dosji')
+            ->select('skripsi.id_skripsi','biodata_mhs.nim_id','biodata_mhs.nama_mhs', 'skripsi.NIM_id', 'kbk.jenis_kbk', 'skripsi.Judul', 'skripsi.tgl_sidangpro', 'skripsi.waktu_sidangpro', 'dosen_pembimbing.nip_id as dosbing','ruang.nama_ruang','dosen_penguji.nip_id as dosji')
             // ->whereNull('skripsi.deleted_at')
             // ->whereNull('nilai_sidangpro')
             //->where('dosen_pembimbing.nip_id','=',$nip)
@@ -62,12 +64,13 @@ class JadwalSidangController extends Controller
                 $dosen1 = $jadwal_sidang_proposal[$i]->dosbing;
                 $dosen2 = $jadwal_sidang_proposal[$i+1]->dosbing;
                 $dosen3 = $jadwal_sidang_proposal[$i]->dosji;
-
+                $id_skripsi = $jadwal_sidang_proposal[$i]->id_skripsi;
 
                 $tmp = array(
                     'id_skripsi'=>$jadwal_sidang_proposal[$i]->id_skripsi,
-                    'nim'=>$jadwal_sidang_proposal[$i]->nim,
+                    'nim'=>$jadwal_sidang_proposal[$i]->nim_id,
                     'NIM_id'=>$jadwal_sidang_proposal[$i]->NIM_id,
+                    'nama_mhs'=>$jadwal_sidang_proposal[$i]->nama_mhs,
                     'jenis_kbk'=>$jadwal_sidang_proposal[$i]->jenis_kbk,
                     'Judul'=>$jadwal_sidang_proposal[$i]->Judul,
                     'tgl_sidangpro'=>$jadwal_sidang_proposal[$i]->tgl_sidangpro,
@@ -78,9 +81,12 @@ class JadwalSidangController extends Controller
                     'ruang'=>$jadwal_sidang_proposal[$i]->nama_ruang
                     );
 
+                $t = $this->cek_duplikat($id_skripsi,$final_result);
                 if(($dosen1==$nip)||$dosen2==$nip||$dosen3==$nip){
+                if($t==0){
                 $final_result[$j] = $tmp;
                 $j++;
+                }
             }
 
                 //array_push($final_result, $tmp);
@@ -96,12 +102,26 @@ class JadwalSidangController extends Controller
         //         'jadwal_sidang_proposal'=>$final_result
 
         //     );
+         $input = array_map("unserialize", array_unique(array_map("serialize", $final_result)));
+        //$final_result = array_unique($input);
 
         $data = array(
             'page'=>'lihat-jadwal-sidang-proposal-mahasiswa',
-            'jadwal_sidang_proposal'=>$final_result
+            'jadwal_sidang_proposal'=>$input
             );
         return view('dosen.monitoring-skripsi.jadwal-sidang.proposal',$data);
+
+    }
+
+    private function cek_duplikat($id_skripsi,$arr)
+    {
+        # code...
+        for($i=0;$i<count($arr);$i++){
+            $tmp_arr = $arr[$i];
+            if($tmp_arr['id_skripsi']==$id_skripsi)
+                return 1;
+        }
+        return 0;
 
     }
 
@@ -117,14 +137,17 @@ class JadwalSidangController extends Controller
         $dosen = DB::table('dosen')->get();
         $petugas_tu = DB::table('petugas_tu')->get();
 
+        $data_skripsi = DB::table('skripsi')->get();
+
+
         $jadwal_sidang_skripsi = DB::table('skripsi')
-            ->leftJoin('mahasiswa', 'skripsi.NIM_id', '=', 'mahasiswa.nim')
+            ->leftJoin('biodata_mhs', 'skripsi.NIM_id', '=', 'biodata_mhs.nim_id')
             ->leftJoin('kbk', 'skripsi.kbk_id', '=', 'kbk.id_kbk')
             ->leftJoin('petugas_tu','skripsi.nip_petugas_id','=','petugas_tu.nip_petugas')
             ->leftJoin('ruang','skripsi.tempat_sidangskrip','=','ruang.id_ruang')
             ->leftJoin('dosen_penguji','dosen_penguji.skripsi_id','=','skripsi.id_skripsi')
             ->leftJoin('dosen_pembimbing','dosen_pembimbing.skripsi_id','=','skripsi.id_skripsi')
-            ->select('skripsi.id_skripsi','mahasiswa.nim', 'skripsi.NIM_id', 'kbk.jenis_kbk', 'skripsi.Judul', 'skripsi.tgl_sidangskrip', 'skripsi.waktu_sidangskrip', 'dosen_pembimbing.nip_id as dosbing','ruang.nama_ruang','dosen_penguji.nip_id as dosji')
+            ->select('skripsi.id_skripsi','biodata_mhs.nim_id','biodata_mhs.nama_mhs', 'skripsi.NIM_id', 'kbk.jenis_kbk', 'skripsi.Judul', 'skripsi.tgl_sidangskrip', 'skripsi.waktu_sidangskrip', 'dosen_pembimbing.nip_id as dosbing','ruang.nama_ruang','dosen_penguji.nip_id as dosji')
             // ->whereNull('skripsi.deleted_at')
             // ->whereNotNull('nilai_sidangpro')
             // ->whereNull('nilai_sidangskrip')
@@ -142,9 +165,12 @@ class JadwalSidangController extends Controller
                 $dosen2 = $jadwal_sidang_skripsi[$i+1]->dosbing;
                 $dosen3 = $jadwal_sidang_skripsi[$i]->dosji;
 
+$id_skripsi = $jadwal_sidang_skripsi[$i]->id_skripsi;
+
                 $tmp = array(
                     'id_skripsi'=>$jadwal_sidang_skripsi[$i]->id_skripsi,
-                    'nim'=>$jadwal_sidang_skripsi[$i]->nim,
+                    'nim'=>$jadwal_sidang_skripsi[$i]->nim_id,
+                    'nama_mhs'=>$jadwal_sidang_skripsi[$i]->nama_mhs,
                     'NIM_id'=>$jadwal_sidang_skripsi[$i]->NIM_id,
                     'jenis_kbk'=>$jadwal_sidang_skripsi[$i]->jenis_kbk,
                     'Judul'=>$jadwal_sidang_skripsi[$i]->Judul,
@@ -156,10 +182,15 @@ class JadwalSidangController extends Controller
                     'ruang'=>$jadwal_sidang_skripsi[$i]->nama_ruang
                     );
 
-                if(($dosen1==$nip)||$dosen2==$nip||$dosen3==$nip){
+                  $t = $this->cek_duplikat($id_skripsi,$final_result);
+
+                     if(($dosen1==$nip)||$dosen2==$nip||$dosen3==$nip){
+                if($t==0){
                 $final_result[$j] = $tmp;
                 $j++;
+                }
             }
+
                 
                 //array_push($final_result, $tmp);
          }
--- resources/views/dosen/monitoring-skripsi/hasil-sidang/skripsi.blade.php
+++ resources/views/dosen/monitoring-skripsi/hasil-sidang/skripsi.blade.php
@@ -4,11 +4,11 @@
 @endsection
 
 @section('htmlheader_title')
-Hasil Sidang Proposal
+Hasil Sidang Skripsi
 @endsection
 
 @section('contentheader_title')
-Hasil Sidang Proposal
+Hasil Sidang Skripsi
 @endsection
 
 @section('main-content')
@@ -22,15 +22,15 @@ Hasil Sidang Proposal
 
   <div class="panel-body">
 
-<div class="box" id="tombol-manage-hasil-proposal" style="display: none;">
+<div class="box" id="tombol-manage-hasil-skripsi" style="display: none;">
 
-<button type="button" class="btn btn-warning" id="tombol-tambah-nilai-hasil-sidang-proposal">Upload Nilai</button>
+<button type="button" class="btn btn-warning" id="tombol-tambah-nilai-hasil-sidang-skripsi">Upload Nilai</button>
 
 <!-- <button type="button" class="btn btn-danger">Hapus</button> -->
 
 </div>
 
-    <table data-toggle="table" id="tabel-hasil-proposal" data-locale="en-US" data-search="true" data-toolbar="#tombol-manage-hasil-proposal"> 
+    <table data-toggle="table" id="tabel-hasil-skripsi" data-locale="en-US" data-search="true" data-toolbar="#tombol-manage-hasil-skripsi"> 
     <thead>
       <tr>
 
@@ -43,7 +43,7 @@ Hasil Sidang Proposal
         <th data-field="dosbing1">Dosen Pembimbing 1</th>
         <th data-field="dosbing2">Dosen Pembimbing 2</th>
         <th data-field="dosji">Dosen Penguji</th>
-        <th data-field="status_skripsi">Status Proposal</th>
+        <th data-field="status_skripsi">Status Skripsi</th>
         <th data-field="nilai">Nilai</th>
       </tr>
     </thead>
@@ -51,40 +51,40 @@ Hasil Sidang Proposal
      
    
 
-      @for ($i = 0; $i < count($hasil_sidang_proposal); $i++)
+      @for ($i = 0; $i < count($hasil_sidang_skripsi); $i++)
                             <tr>
                                 <!-- Task Name -->
                                 <td></td>
                                 <td>
-                                  {{$hasil_sidang_proposal[$i]['id_skripsi']}}
+                                  {{$hasil_sidang_skripsi[$i]['id_skripsi']}}
                                 </td>
                                 <td>
-                                    {{ $hasil_sidang_proposal[$i]['nim']}}
+                                    {{ $hasil_sidang_skripsi[$i]['nim']}}
                                 </td>
                                 <td>
-                                    {{ $hasil_sidang_proposal[$i]['nama_mhs']}}
+                                    {{ $hasil_sidang_skripsi[$i]['nama_mhs']}}
                                 </td>
                                 <td>
-                                    <div>{{ $hasil_sidang_proposal[$i]['jenis_kbk']}}</div>
+                                    <div>{{ $hasil_sidang_skripsi[$i]['jenis_kbk']}}</div>
                                 </td>
                                 <td>
-                                    <div>{{ $hasil_sidang_proposal[$i]['Judul']}}</div>
+                                    <div>{{ $hasil_sidang_skripsi[$i]['Judul']}}</div>
                                 </td>
                                
                                 <td>
-                                    <div>{{ $hasil_sidang_proposal[$i]['dosen_pembimbing1']}}</div>
+                                    <div>{{ $hasil_sidang_skripsi[$i]['dosen_pembimbing1']}}</div>
                                 </td>
                                 <td>
-                                    <div>{{ $hasil_sidang_proposal[$i]['dosen_pembimbing2']}}</div>
+                                    <div>{{ $hasil_sidang_skripsi[$i]['dosen_pembimbing2']}}</div>
                                 </td>
                                 <td>
-                                    <div>{{ $hasil_sidang_proposal[$i]['dosen_penguji']}}</div>
+                                    <div>{{ $hasil_sidang_skripsi[$i]['dosen_penguji']}}</div>
                                 </td>
                                 <td>
-                                    <div>{{ $hasil_sidang_proposal[$i]['status_proposal']}}</div>
+                                    <div>{{ $hasil_sidang_skripsi[$i]['status_skripsi']}}</div>
                                 </td>
                                <td>
-                                    <div>{{ $hasil_sidang_proposal[$i]['nilai_proposal']}}</div>
+                                    <div>{{ $hasil_sidang_skripsi[$i]['nilai_skripsi']}}</div>
                                 </td>
                                 </tr>
 @endfor
@@ -97,25 +97,25 @@ Hasil Sidang Proposal
 </div>
 
 <!-- Modal -->
-<div id="modal-upload-hasil-sidang-proposal" class="modal fade" role="dialog">
+<div id="modal-upload-hasil-sidang-skripsi" class="modal fade" role="dialog">
   <div class="modal-dialog">
 
     <!-- Modal content-->
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Upload Hasil Sidang Proposal</h4>
+        <h4 class="modal-title">Upload Hasil Sidang Skripsi</h4>
       </div>
       <div class="modal-body">
-        <input type="hidden" id="hasil_sidang_proposal_id"></input>
+        <input type="hidden" id="hasil_sidang_skripsi_id"></input>
         <div class="form-group">
-        <label for="tambah-nilai-sidang-proposal">Nilai Sidang Proposal</label>
-        <input type="number" class="form-control" id="tambah-nilai-sidang-proposal"></input>
+        <label for="tambah-nilai-sidang-skripsi">Nilai Sidang Skripsi</label>
+        <input type="number" class="form-control" id="tambah-nilai-sidang-skripsi"></input>
         </div>
         
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" id="tombol-sumbit-tambah-nilai-sidang-proposal">Upload</button>
+        <button type="button" class="btn btn-default" id="tombol-sumbit-tambah-nilai-sidang-skripsi">Upload</button>
       </div>
     </div>
 
@@ -124,32 +124,32 @@ Hasil Sidang Proposal
 
   <script type="text/javascript">
       $(document).ready(function(){
-            $('#tombol-tambah-nilai-hasil-sidang-proposal').click(function(){
+            $('#tombol-tambah-nilai-hasil-sidang-skripsi').click(function(){
                     //alert('tet');
-                    var id_skripsi = $('#tabel-hasil-proposal').bootstrapTable('getSelections')[0].id_skripsi;
+                    var id_skripsi = $('#tabel-hasil-skripsi').bootstrapTable('getSelections')[0].id_skripsi;
 
                     id_skripsi = id_skripsi.trim();
 
-                    $('#hasil_sidang_proposal_id').val(id_skripsi);
+                    $('#hasil_sidang_skripsi_id').val(id_skripsi);
 
-                    $('#modal-upload-hasil-sidang-proposal').modal('show');
+                    $('#modal-upload-hasil-sidang-skripsi').modal('show');
 
                     //alert(id_skripsi);
             });
 
-            $('#tombol-sumbit-tambah-nilai-sidang-proposal').click(function(){
+            $('#tombol-sumbit-tambah-nilai-sidang-skripsi').click(function(){
 
-                var id_skripsi = $('#hasil_sidang_proposal_id').val();
+                var id_skripsi = $('#hasil_sidang_skripsi_id').val();
                 
-                var nilai_sidang_proposal = $('#tambah-nilai-sidang-proposal').val();
+                var nilai_sidang_skripsi = $('#tambah-nilai-sidang-skripsi').val();
 
                 $.ajax({
-                     url: 'upload-nilai-sidang-proposal',
+                     url: 'upload-nilai-sidang-skripsi',
                     type: "post",
                     data: {"_token": "{{ csrf_token() }}",
                         "id_skripsi":id_skripsi,
 
-                        "nilai_sidang_proposal":nilai_sidang_proposal
+                        "nilai_sidang_skripsi":nilai_sidang_skripsi
                     },
                     success: function(response){
                         if(response.status_upload==1){
